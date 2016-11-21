50d242ac42b7e30c7528e3f9db09824d0369461f
ratikagrawal <ratikmastiboy@gmail.com>
2016-11-22 00:22:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 });
 app.get('/home',function(req,res){
     res.sendFile(path.join(__dirname,'ui','homepage.html'));
-})
+});
 app.get('/article-Fourth', function (req, res) {
   res.send('Welcome to Page 1');
 });
