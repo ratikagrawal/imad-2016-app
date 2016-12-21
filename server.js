6c6d30db80c26fcadcede44d7977d3bc62ab3086
ratikagrawal <ratikmastiboy@gmail.com>
2016-12-22 01:04:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-
-var app = express();
-app.use(morgan('combined'));
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'homepage.html'));
 });
